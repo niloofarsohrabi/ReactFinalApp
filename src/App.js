ddd9b48ee67519461033a0245028cf1eab383e4c
niloofarsohrabi <nilufar.sohrabi@gmail.com>
2019-12-28 10:20:45 +0330

    first

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+export default class App extends Component {
+    render() {
+        return (
+            <div>
+                 <header className="App-header">
+                <h1 className="App-title">Welcome to my web store-project</h1>
+                </header>
+            </div>
+        )
+    }
+}
+
+
+

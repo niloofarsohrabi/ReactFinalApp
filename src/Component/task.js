0d17b33a948d8fbca061209323ea3028fe8d14ab
niloofarsohrabi <nilufar.sohrabi@gmail.com>
2019-12-28 10:27:53 +0330

    third

---
+++ src/Component/task.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class task extends Component {
+    render() {
+        return (
+            <div>
+                <h1>task</h1>
+            </div>
+        )
+    }
+}
